31665a29759810595897675ee1a0b1faf0c8aa15
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-05-11 12:47:41 +0100

    adds raise and lower temp functions

--- spec/features/ThermostatSpec.js
+++ spec/features/ThermostatSpec.js
@@ -1,10 +1,32 @@
 
 describe( 'Thermostat', function(){
 	var thermostat;
-	describe( 'default settings', function(){
+
+	beforeEach(function() {
 		thermostat = new Thermostat();
+	});
+
+	describe( 'default settings', function(){
 		it ('starts at 20 degrees', function(){
-			expect(thermostat.targetTemp()).toEqual(20)
+			expect(thermostat.returnTemp()).toEqual(20)
+		});
+	});
+  describe('functions', function(){
+		it ('can increase the temperature by 1 degree', function(){
+			thermostat.raiseTemp();
+			expect(thermostat.returnTemp()).toEqual(21)
+		});
+		it ('can decrease the temperature by 1 degree', function(){
+			thermostat.lowerTemp();
+			expect(thermostat.returnTemp()).toEqual(19)
+		});
+	});
+	describe('parameters', function(){
+		it ('has a minimum temperature of 10 degrees', function(){
+			for (i = 0; i < 11; i++){
+				thermostat.lowerTemp();
+			}
+			expect(thermostat.returnTemp()).toEqual(10)
 		});
 	});
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,8 +1,23 @@
 
 function Thermostat() {
 	this._targetTemp = 20
+	this.MIN_TEMP = 10
 };
 
-Thermostat.prototype.targetTemp = function() {
+Thermostat.prototype.returnTemp = function() {
 	return this._targetTemp;
 };
+
+Thermostat.prototype.raiseTemp = function() {
+	return this._targetTemp += 1
+}
+
+Thermostat.prototype.lowerTemp = function() {
+	if (this.isMinTemp() > this._targetTemp);
+	return;
+	this._targetTemp -= 1
+}
+
+Thermostat.prototype.isMinTemp = function() {
+	this === this.MIN_TEMP;
+}
